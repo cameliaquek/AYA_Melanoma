d640d1810c12342d64566004a2c59359df3b84fb
cathxinyubai <95009365+cathxinyubai@users.noreply.github.com>
2024-02-12 16:19:48 +1100

    Update 05_AYA_scoreROC.R

--- 05_AYA_scoreROC.R
+++ 05_AYA_scoreROC.R
@@ -1,3 +1,88 @@
+devtools::install_github('DavisLaboratory/singscore')
+
+setwd("/Users/xinyubai/Documents/01_Research projects/AYA_immunotherapy/02analysis/01NR_G1G2_analysis/Gene-signatures_singscore/tcga_singscore")
+library(singscore)
+library(GSEABase)
+library(dplyr)
+library(ggplot2)
+
+
+aya <- read.table("aya28_singscore_df.txt", header = 1, row.names = 1)
+#aya$AYA4 <- NULL
+#aya$AYA5 <- NULL
+#aya$AYA6 <- NULL
+#aya$AYA28 <- NULL
+head(aya,6)
+
+aya<- read.table("aya47_lognorm.txt", header = 1, row.names = 1)
+tcga <- aya [,1:19]
+
+gUP <- read.table("upreg_YIM_genes.txt", header = 1)[,1]
+gDN <- read.table("downreg_YIM_genes.txt", header = 1)[,1]
+
+
+# The recommended method for dealing with ties in ranking is 'min', you can
+# change by specifying 'tiesMethod' parameter for rankGenes function.
+rankData <- rankGenes(aya, tiesMethod = "min")
+rankData <- rankGenes(tcga, tiesMethod = "min")
+
+# Given the ranked data and gene signature, simpleScore returns the scores and 
+# dispersions for each sample
+scoredf <- simpleScore(rankData, upSet = gUP, downSet = gDN)
+write.table(scoredf, 
+            file="aya28_YIM_singscore2.txt", 
+            sep="\t", col.names = NA)
+
+write.table(scoredf,
+            file = "tcga_YIM_singscore",
+            sep="\t", col.names = NA)
+
+
+
+# Note that, when only one gene set is available in a gene signature, one can 
+# only input values for the upSet argument. In addition, a knownDirection 
+# argument can be set to FALSE if the direction of the gene set is unknown.
+ipres <- read.table("ipres.txt",header = T)
+ipres <- unique(ipres)
+ipres <- ipres$gene
+scoredf_ipres <- simpleScore(rankData, upSet = ipres, knownDirection = FALSE)
+
+
+write.table(scoredf_ipres, 
+            file="aya28_IPRESsingscore.txt", 
+            sep="\t", col.names = NA)
+###
+write.table(scoredf_ipres, 
+            file="tcga_IPRESsingscore.txt", 
+            sep="\t", col.names = NA)
+
+immuUP <- read.table("immuUP.txt", header = 1)[,1]
+immuDN <- read.table("immuDN.txt", header = 1)[,1]
+
+scoredf_immu <- simpleScore(rankData, upSet = immuUP, downSet = immuDN)
+##
+write.table(scoredf_immu, 
+            file="aya28_IMMUsingscore.txt", 
+            sep="\t", col.names = NA)
+##
+write.table(scoredf_immu, 
+            file="tcga_IMMUsingscore.txt", 
+            sep="\t", col.names = NA)
+
+#  Get the annotations of samples by their sample names
+ayaAnnot <- read.table("aya_Annot.txt", header = 1)
+# Sample annotations
+ayaAnnot$Type
+
+png("AYAsingscore_dispersion.png")
+plotDispersion(scoredf,annot = ayaAnnot$Type,isInteractive = FALSE)
+dev.off
+
+scoreLS <- plotScoreLandscape(scoredf, scoredf_ipres, 
+                   scorenames = c('IPRES', 'G1G2_DE'),hexMin = 8)
+
+
+
 #######################################
 # ROC analysis
 #######################################
