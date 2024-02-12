0c115de6365ea58cd07e592fd75ae736f94d94ce
cathxinyubai <95009365+cathxinyubai@users.noreply.github.com>
2024-02-12 13:32:19 +1100

    Update 05_AYA_scoreROC.R

--- 05_AYA_scoreROC.R
+++ 05_AYA_scoreROC.R
@@ -18,19 +18,6 @@ roc1; roc2; roc3 ##get AUC values
 
 
 #########################################################
-# 先绘制1条ROC曲线
-plot(roc1, 
-     print.auc=TRUE, # 图像上输出AUC的值
-     print.auc.x=0.4, print.auc.y=0.5, # 设置AUC值坐标为（x，y）
-     #auc.polygon=TRUE, # 将ROC曲线下面积转化为多边形
-     #auc.polygon.col="#fff7f7",  # 设置ROC曲线下填充色
-     #grid=c(0.5, 0.2), # 设置两轴网格线的间隔为0.5，0.2
-     #grid.col=c("black", "black"),  # 设置两轴间隔线条的颜色
-     #print.thres=TRUE, # 图像上输出最佳截断值
-     main="Gene signatures for prediction of AYA poor immunogenicity",  # 添加图形标题
-     col="#fc8d62",    # 设置ROC曲线颜色
-     #legacy.axes=TRUE  # 使x轴从0到1，表示为1-特异度
-     percent=TRUE)   
 
 # Save plots
 png("AYA28gene-signatures_ROC_suppl.png", unit = "cm", width = 15, height = 15, res = 500)
